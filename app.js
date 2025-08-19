48fab3704700d9ae9956cf14010f4d7f3c2c70b3
SR <saodat.riskiyeva@gmail.com>
2025-08-18 23:46:26 -0400

    attaching js to html

---
+++ app.js
@@ -0,0 +1,11 @@
+let age = 43;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Software Engineer",
+  place: "New York",
+  salary: 120000,
+};
+alert(age);
+alert(userName);
+alert(hobbies[0]);
